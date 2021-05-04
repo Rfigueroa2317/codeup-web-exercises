e4027b23880732ab7d68067e17a3c2bd7beb3cf8
ricardofigueroa <rfigueroa2317@gmail.com>
2021-05-04 09:28:38 -0500

    daily push

---
+++ js/daily-warmup-may-4.js
@@ -0,0 +1,21 @@
+
+
+
+// Write a function named cube(n) that returns n * n * n
+
+        function cube(input){
+            return input * input * input;
+        }
+        console.log(cube(3))
+
+
+
+
+
+// Write a function named cubeRoot(n) that returns the cube root of the input
+
+
+        function cubeRoot(input){
+            return Math.cbrt(input);
+        }
+        console.log(Math.cbrt(27))
