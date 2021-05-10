13b28a835459104f2ffe935e14e12e225a4580d6
ricardofigueroa <rfigueroa2317@gmail.com>
2021-05-09 20:14:48 -0500

    update

--- js/planets-string.js
+++ js/planets-string.js
@@ -1,4 +1,4 @@
-(function(){
+(function () {
     "use strict";
 
     // var planetsString = "Mercury|Venus|Earth|Mars|Jupiter|Saturn|Uranus|Neptune";
@@ -9,8 +9,8 @@
      * planetsArray.
      * console.log planetsArray to check your work
      */
-        // var planetsArray = planetsString.split( "");
-        // console.log(planetsArray);
+    // var planetsArray = planetsString.split( "");
+    // console.log(planetsArray);
 
     /**
      * TODO:
@@ -18,23 +18,31 @@
      * results. Why might this be useful?
      */
 
-        var str = `{<br>}Mercury{</br>} , {<br>}Venus{</br>} , {<br>}Earth{</br>} , {<br>} Mars{</br>} , {<br>}Jupiter{</br>} , {<br>}Saturn{</br>} , {<br>}Uranus{</br>} , {<br>}Neptune{</br>}`
+    // var str = `{<br>}Mercury{</br>} , {<br>}Venus{</br>} , {<br>}Earth{</br>} , {<br>} Mars{</br>} , {<br>}Jupiter{</br>} , {<br>}Saturn{</br>} , {<br>}Uranus{</br>} , {<br>}Neptune{</br>}`
+    //
+    // console.log(str + `{<br>}`);
+    //
+    //
+    // console.log('--------------------------------------------------')
 
-            console.log(str + `{<br>}`);
-
-
-     /**
-      BONUS:
+    /**
+     BONUS:
      * Create another string that would display your planets in an undordered
      * list. You will need an opening AND closing <ul> tags around the entire
      * string, and <li> tags around each planet.
      */
 
-
-
-
-
-
+    var newStr = `{<ul>}
+                         {<Li>Mercury</Li>}
+                         {<Li>Venus</Li>}
+                         {<Li>Earth</Li>}
+                         {<Li>Mars</Li>}
+                         {<Li>Jupiter</Li>}
+                         {<Li>Saturn</Li>}
+                         {<Li>Uranus</Li>}
+                         {<Li>Neptune</Li>}
+                 {</ul>}`;
+    console.log(newStr);
 
 
 })();
