1e1945d9c5fa1f524663bbeb52f800763008e07f
ricardofigueroa <rfigueroa2317@gmail.com>
2021-05-11 07:15:58 -0500

    daily warmup

---
+++ js/daily-warmup-may-11.js
@@ -0,0 +1,32 @@
+//  Function warmup 5/11 //
+
+
+
+//Make a function named `isTrue(boolean)`
+        function isTtrue(bool){
+            return true;
+        }
+
+        //console.log(isTtrue())
+
+//Make a function named `isFalse(boolean)`
+        function isFalse(bool){
+            return false;
+        }
+        //console.log(isFalse())
+
+
+//Make a function named `isTruthy(input)`, remember that values other than true
+//  will behave like true
+
+        function isTruthy(input){
+            return
+        }
+
+
+//Make a function named `isFalsy(input)`, remember that values other than false
+//  behave like false
+
+        function isFalsy(input){
+
+        }
