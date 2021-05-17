2c6985d81410fb5afb757f79c714f26fa9c048b6
ricardofigueroa <rfigueroa2317@gmail.com>
2021-05-17 09:25:58 -0500

    update

---
+++ js/daily-warmup-may-17.js
@@ -0,0 +1,11 @@
+// Write a function named secondToLast(arr) that
+// returns the data type
+// of the second to last element of the array parameter.
+// When you call the function, pass in an array of your choosing
+
+function secondToLast(){
+    var arr = ['apple', 'lemon', 'orange', 'mango']
+    return typeof arr.indexOf();
+}
+
+console.log(secondToLast.length('orange'))
