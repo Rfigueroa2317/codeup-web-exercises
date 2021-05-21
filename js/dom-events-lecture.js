1d6b54290d0228bafda374d786e7019db6156ecb
ricardofigueroa <rfigueroa2317@gmail.com>
2021-05-21 12:23:47 -0500

    daily push

---
+++ js/dom-events-lecture.js
@@ -0,0 +1,24 @@
+const textOutput = document.getElementById("text-output")
+
+
+// addInputEventListener();
+//
+// function addInputEventListener(){
+//     document.getElementById("user-input")
+//         .addEventListener("input", getInputListener)
+// }
+//
+// function getInputListener(){
+//     console.log(this.value);
+//     // document.getElementById("text-output")
+//         textOutput.textContent = this.value;
+// }
+
+function addButtonEvent(){
+    document.getElementById("submitBtn").addEventListener("click", addPost)
+}
+
+function addPost(){
+
+
+}
