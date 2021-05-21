816f1c12607b355f06bc7f49cc05be19a0d99c05
ricardofigueroa <rfigueroa2317@gmail.com>
2021-05-21 14:55:43 -0500

    daily push

--- js/dom-events.js
+++ js/dom-events.js
@@ -1,10 +1,21 @@
-
 function addButtonEvent() {
     document.getElementById("submitBtn")
-            .addEventListener("click", addPost)
+        .addEventListener("click", addPost)
 }
 
 function addPost() {
-
-
+   var textArea = document.getElementById("user-input")
+    var newPost = document.createElement("textarea");
+    newPost.innerText = textArea.value
+    document.getElementById("blog-container").prepend(newPost)
 }
+
+
+function addReply() {
+    var textArea = document.getElementById("reply")
+    var newReply = document.createElement("textarea");
+    newReply.innerText = textArea.value
+    document.getElementById("blog-container").prepend(newReply)
+}
+
+addButtonEvent();
