00ad2746ac7fd5e0de917aa9541b3a916d4b93fc
ricardofigueroa <rfigueroa2317@gmail.com>
2021-05-24 22:14:12 -0500

    update

--- js/dom-events-practice.js
+++ js/dom-events-practice.js
@@ -1,24 +1,49 @@
 // DOM practice \\
 
+// function changeColor(obj) {
+//     obj.style.color = "Peru";
+//}
 function changeColor(obj) {
-    obj.style.color = "Peru";
+    obj.style.color = 'purple';
 }
+//
+// document.getElementById("myBtn").onclick = changeBackgroundColorToMediumOrchid;
+    document.getElementById("myBtn").onclick = changeBackgroundColorToLightBlue;
+//
+// function changeBackgroundColorToMediumOrchid(){
+//     document.querySelector('body').style.background = "MediumOrchid";
+// }
+    function changeBackgroundColorToLightBlue(){
+        document.querySelector('body').style.background ='LightBlue';
+    }
+// function removeLetterFromH1(){
+//     var h1 = document.querySelector('h1')
+//     h1.textContent = h1.textContent.slice(0, -1);
+// }
+    function addLetterToH1(){
+        var h1 = document.querySelector('h1')
+        h1.textContent = h1.textContent.slice(0, 1);
+    }
+//
+// function mOver(obj) {
+//     obj.innerHTML = "GET OFF ME!"
+// }
+    function mOver(obj){
+        obj.innerHTML = "Hello There!";
+    }
+//
+// function mOut(obj) {
+//     obj.innerHTML = "thank you"
+//}
+    function mOut(obj){
+        obj.innerHTML = 'have a great day!';
+    }
 
-document.getElementById("myBtn").onclick = changeBackgroundColorToMediumOrchid;
+    var input = document.querySelector('input');
+    var log = document.getElementById('values');
 
-function changeBackgroundColorToMediumOrchid(){
-    document.querySelector('body').style.background = "MediumOrchid";
-}
+    input.addEventListener('input', updateValue)
 
-function removeLetterFromH1(){
-    var h1 = document.querySelector('h1')
-    h1.textContent = h1.textContent.slice(0, -1);
-}
-
-function mOver(obj) {
-    obj.innerHTML = "GET OFF ME!"
-}
-
-function mOut(obj) {
-    obj.innerHTML = "thank you"
+function updateValue(e){
+    log.textContent = e.target.value;
 }
