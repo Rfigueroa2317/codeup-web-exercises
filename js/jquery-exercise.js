e8b2a0ef28c83d6dd954f46f6df6f0f89c011ad8
ricardofigueroa <rfigueroa2317@gmail.com>
2021-06-01 15:22:09 -0500

    update

--- js/jquery-exercise.js
+++ js/jquery-exercise.js
@@ -1,11 +1,25 @@
+// todo #1 \\
 $('#all-caps').hide();
 
 $('span').hide();
 
 $('.show-me').show();
 
+// todo #2 \\
+
 $('#pink').css('background-color','pink');
 
 $('#orange').css('background-color','orange');
 
 $('#pink').hide();
+
+// curriculum exercises \\
+
+// $('#exercises').show();
+// alert('this is the exercises id.');
+
+$('#line-1').show();
+
+// $('#header').show();
+// alert('this is the header id.');
+
