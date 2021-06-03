0fca7d5e9341602dad6ac14c25fc41ee6bd1e2e3
ricardofigueroa <rfigueroa2317@gmail.com>
2021-06-03 14:35:37 -0500

    update

--- js/jquery_exercises.js
+++ js/jquery_exercises.js
@@ -1,15 +1,15 @@
 // todo #1 \\
-$('#all-caps').hide();
-
-$('span').hide();
-
-$('.show-me').show();
-
-// todo #2 \\
-
-$('#pink').css('background-color','pink');
-
-$('#orange').css('background-color','orange');
+// $('#all-caps').hide();
+//
+// $('span').hide();
+//
+// $('.show-me').show();
+//
+// // todo #2 \\
+//
+// $('#pink').css('background-color','pink');
+//
+// $('#orange').css('background-color','orange');
 //
 // $('#pink').hide();
 
@@ -55,12 +55,52 @@ $('#orange').css('background-color','orange');
 
 // look for ALL h1 AND all li and all p
 // $(h1, p, li)
-$('* h1, * p, * li').css({
-    'color': 'blue',
-});
+// $('* h1, * p, * li').css({
+//     'color': 'blue',
+// });
+//
+//
+//
+// $('div p') // all paragraph elements inside of any div element
+// $('div, p') // all divs AND all paragraphs
+// $('div ul li') // ALL li's inside of any ul only if that ul is inside of any div
 
 
+// ======================================================= \\
 
-$('div p') // all paragraph elements inside of any div element
-$('div, p') // all divs AND all paragraphs
-$('div ul li') // ALL li's inside of any ul only if that ul is inside of any div
+//  *** MOUSE EVENTS ***  \\
+
+// .2
+
+$('#header').click(
+    function () {
+        $(this).css('background-color', 'lightblue');
+    })
+
+
+// .3
+
+$('div, p').dblclick(
+    function () {
+        $(this).css('font-size', '18px');
+    })
+
+
+// .4
+
+// $('ul, li').mouseenter(function (){
+//         $(this).css('color','red');
+// });
+//
+// $('ul, li').mouseleave(function (){
+//         $(this).css('color','black');
+// });
+
+$(' li').hover(
+    function () {
+        $(this).css('color', 'red');
+    },
+    function () {
+        $(this).css('color', 'black');
+    }
+)
