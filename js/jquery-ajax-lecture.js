5d25084138195de24d69bbab9edac941cdfc49b3
ricardofigueroa <rfigueroa2317@gmail.com>
2021-06-08 12:27:53 -0500

    udpate

--- js/jquery-ajax-lecture.js
+++ js/jquery-ajax-lecture.js
@@ -51,4 +51,21 @@
 
 // TODO: using https://jsonplaceholder.typicode.com/ make an ajax request to the comments endpoint and console it.
 
-$.ajax("https://jsonplaceholder.typicode.com/comments",[])
+console.log("/https://jsonplaceholder.typicode.com/comments");
+
+// [
+//     {
+//         "postId": 1,
+//         "id": 1,
+//         "name": "id labore ex et quam laborum",
+//         "email": "Eliseo@gardner.biz",
+//         "body": "laudantium enim quasi est quidem magnam voluptate ipsam eos\ntempora quo necessitatibus\ndolor quam autem quasi\nreiciendis et nam sapiente accusantium"
+//     }
+// ]
+console.log($.ajax("/https://jsonplaceholder.typicode.com/comments"));
+
+
+// TODO: make a POST request to the '/posts' endpoint, make sure to include any data required for that post to be made successfully
+
+
+// TODO: what status do you get back when making a GET request to: https://jsonplaceholder.typicode.com/comments
