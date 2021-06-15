40c463b0e343784f324de02976e14b918cb1f9d2
ricardofigueroa <rfigueroa2317@gmail.com>
2021-06-15 16:08:35 -0500

    update

--- js/promises-lecture.js
+++ js/promises-lecture.js
@@ -48,14 +48,37 @@
 
 // fetch syntax for post request
 
-fetch('https://jsonplaceholder.typicode.com/posts/',{
-    method: "POST",
-    headers: {},
-    body: {title: "Here's a title", body: "here's the body"}
-})
-    .then(response => console.log(response))
-    .catch(err => {
-     console.log('error: ' + err);
- });
+// fetch('https://jsonplaceholder.typicode.com/posts/',{
+//     method: "POST",
+//     headers: {},
+//     body: {title: "Here's a title", body: "here's the body"}
+// })
+//     .then(response => console.log(response))
+//     .catch(err => {
+//      console.log('error: ' + err);
+//  });
 
 // TODO: using fetch(), make a simple GET request to this api: “https://dog.ceo/api/breeds/image/random” and append this image to the image element with an id of “random-dog-image”
+
+fetch("https://dog.ceo/api/breeds/image/random", {
+
+}).then(response => response.json())
+    .then(data => {
+        console.log(data);
+        $("#random-dog-image").attr("src", data.message)
+        console.log("place holder");
+    }).catch(error => console.log(error));
+
+// basic fetch syntax
+// fetch('https://jsonplaceholder.typicode.com/posts/1')
+//     // .then(response => {
+//     //     console.log("inside first then method")
+//     //     console.log(response.json())
+//     // })
+//     // .then(data => {
+//     //     console.log(data);
+//     .then(response => response.json())
+//     .then(data => {
+//         console.log(data);
+//         $("div").append(`<p>${data.title}</p>`);
+//     })
